c598f3f7d5f5f32d3424f1488b9a84af0d5ce506
Thomas Kowark <thomas.kowark@googlemail.com>
2014-10-07 09:26:08 +0200

    list-based loading of rules for graph classification. initial version of node-relation-node rule. TODO: find a less programming based way of defining rules...

--- app/models/correspondence_extractor.rb
+++ app/models/correspondence_extractor.rb
@@ -1,44 +1,54 @@
 class CorrespondenceExtractor
   attr_accessor :rule_engine
   
-  def initialize()
-    reset!  
-  end
+  CLASSIFICATION_RULES = [:simple_graph]#, :node_relation_node, :node_attribute]
   
   def extract_correspondences!(input_graph)
+    reset!
     input_graph.each{|stmt| rule_engine << stmt}
-  end
-  
-  def productions_for(rule_name)
-    rule_engine.productions[rule_name]
   end
   
   def reset!
     @rule_engine = Wongi::Engine.create
-    install_classification_ruleset! 
+    install_classification_rule_set!
   end
   
-  private
+  # install the rules to classify the input and output graph
+  def install_classification_rule_set!
+    CLASSIFICATION_RULES.each{|cr| rule_engine << self.send("#{cr}_rule".to_sym)}
+  end
   
-  # classifies the input and output graphs
-  def install_classification_ruleset!
-    rule_engine << ruleset do
-      rule("simple_graph") do
-        forall {
-          has(:Graph_element, Vocabularies::GraphPattern.belongsTo, :Graph)
-          none {
-            has(:Different_element, Vocabularies::GraphPattern.belongsTo, :Graph)
-            diff(:Graph_element, :Different_element)
-          }
+  # determine whether a graph only has one element
+  def simple_graph_rule()
+    rule("simple_graph") do 
+      forall {
+        has(:Graph_element, Vocabularies::GraphPattern.belongsTo, :Graph)
+        none {
+          has(:Different_element, Vocabularies::GraphPattern.belongsTo, :Graph)
+          diff(:Graph_element, :Different_element)
         }
-        make {
-          gen(:Graph, "classification", "simple")
-        }
-      end
+      }
+      make {
+        gen(:Graph, "classification", "simple")
+      }
     end
   end
   
-  # extracts the correspondences
-  def install_extraction_ruleset!
+  def node_relation_node_rule()
+    rule("n-r-n") do
+      forall {
+        has(:Relation, Vocabularies::GraphPattern.belongsTo, :Graph)
+        has(:Relation, RDF.type, Vocabularies::GraphPattern.RelationConstraint)
+        has(:Node, Vocabularies::GraphPattern.outgoingRelation, :Relation)
+        has(:Node2, Vocabularies::GraphPattern.incomingRelation, :Relation)
+        none {
+          has(:Diff, Vocabularies::GraphPattern.belongsTo, :Graph)
+          # different thatn Node, Node2, or Relation
+        }
+      }
+      make {
+        gen(:Graph, "classification", "simple")
+      }
+    end
   end
 end
