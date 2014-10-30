142e43a7c8fabd28be7759df131ed1f8a3b95e57
Thomas Kowark <thomas.kowark@googlemail.com>
2014-10-30 16:29:24 +0100

    Testing Path creation

--- spec/models/ontology_matcher_spec.rb
+++ spec/models/ontology_matcher_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe OntologyMatcher, :type => :model do
     Pattern.any_instance.stub(:initialize_repository! => true)
     @pattern = FactoryGirl.create(:pattern)    
     @ontology = FactoryGirl.create(:ontology, :url => "http://example.org/myOntology2")    
-    @om = OntologyMatcher.new(@pattern, [@ontology])    
+    @om = OntologyMatcher.new(@pattern, [@ontology])
   end
   
   it "should not call the matcher when an existing file is present" do
@@ -82,6 +82,7 @@ RSpec.describe OntologyMatcher, :type => :model do
     @pattern.ontologies = [o1]
     @om = OntologyMatcher.new(@pattern, [o2])
     File.delete(aml_test_file) if File.exists?(aml_test_file)
+    assert_equal true, @om.alignment_path(o1,o2).ends_with?("ont_#{o1.id}_ont_#{o2.id}.rdf")
     @om.stub(:alignment_path => aml_test_file)    
     @om.match!
     assert_equal true, File.exists?(aml_test_file)
