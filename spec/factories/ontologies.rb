a19f63288983656df1e2f66f2851a142e8c76c6a
Thomas Kowark <thomas.kowark@googlemail.com>
2014-11-07 17:33:02 +0100

    Resolving URL conflicts in Ontology Factory

--- spec/factories/ontologies.rb
+++ spec/factories/ontologies.rb
@@ -1,7 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :url do |n|
+    "http://example.org/ontology_#{n}"
+  end
+  
   factory :ontology do
-    url "http://example.org/myOntology"
+    url
   end
 end
